19405d9f15475de90577f33c0c6cd775a27cc299
Samuel Gomez <contact@samuelgomez.fr>
2022-09-25 18:31:40 +0200

    feat(api): get local data api

--- src/pages/blog/index.page.tsx
+++ src/pages/blog/index.page.tsx
@@ -1,22 +1,32 @@
 import type { GetStaticProps } from 'next';
 import Posts, { PostsProps } from './Posts';
 
-const Blog = ({ posts = [] }: PostsProps) => {
+export interface TPerson {
+  person: {
+    name?: string;
+  };
+}
+
+const Blog = ({ posts = [], person = {} }: PostsProps & TPerson) => {
   return (
     <div>
-      <p>My blog</p>
+      <p>My blog {person?.name}</p>
       <Posts posts={posts} />
     </div>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
+  const resPerson = await fetch('http://localhost:3000/api/hello');
+  const person = await resPerson.json();
+
   const result = await fetch('https://jsonplaceholder.typicode.com/posts');
   const posts = await result.json();
 
   return {
     props: {
       posts,
+      person,
     },
   };
 };
--- test-utils/server-handlers.ts
+++ test-utils/server-handlers.ts
@@ -5,6 +5,9 @@ const handlers = [
   rest.get('https://jsonplaceholder.typicode.com/posts', (_req, res, ctx) => {
     return res(ctx.json(mockedPosts));
   }),
+  rest.get('http://localhost:3000/api/hello', (_req, res, ctx) => {
+    return res(ctx.json({ name: 'John Doe' }));
+  }),
 ];
 
 export { handlers };
